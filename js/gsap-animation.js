bc0de1f084bc8ae4fab456be25fea1a5b4afb9de
rami <rami.ahmad.abdullah@gmail.com>
2025-12-07 15:59:20 +0600

    2

---
+++ js/gsap-animation.js
@@ -0,0 +1,29 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to('.box-2', {
+        scrollTrigger: {
+            trigger: '.box-2',
+            start: 'top 80%',
+            toggleActions: 'restart none none none',
+        }, // start the animation when ".box" enters the viewport (once)
+
+        pin: true, // pin the element while it's in the viewport
+        scale: .6,
+        duration: 1,
+    });
+
+    scrollTrigger.create({
+        trigger: '.box-3',
+        start: 'top 80%',
+        onEnter: () => {
+            gsap.to('.box-3', {
+                pin: true,
+                scale: .6,
+                duration: 1,
+            });
+        },
+    });
+});
--- js/landing.js
+++ js/landing.js
@@ -18,29 +18,29 @@ window.addEventListener("load", function () {
     video.addEventListener("mouseenter", (e) => {
       video.querySelector("video").play();
 
-      if (video.classList.contains("v1")) {
-        logo.classList.add("golden-shadow");
-      } else if (video.classList.contains("v2")) {
-        logo.classList.add("blue-shadow");
-      } else if (video.classList.contains("v3")) {
-        logo.classList.add("gray-shadow");
-      } else if (video.classList.contains("v4")) {
-        logo.classList.add("brown-shadow");
-      }
+      // if (video.classList.contains("v1")) {
+      //   logo.classList.add("golden-shadow");
+      // } else if (video.classList.contains("v2")) {
+      //   logo.classList.add("blue-shadow");
+      // } else if (video.classList.contains("v3")) {
+      //   logo.classList.add("gray-shadow");
+      // } else if (video.classList.contains("v4")) {
+      //   logo.classList.add("brown-shadow");
+      // }
     });
 
     video.addEventListener("mouseleave", (e) => {
       video.querySelector("video").pause();
 
-      if (video.classList.contains("v1")) {
-        logo.classList.remove("golden-shadow");
-      } else if (video.classList.contains("v2")) {
-        logo.classList.remove("blue-shadow");
-      } else if (video.classList.contains("v3")) {
-        logo.classList.remove("gray-shadow");
-      } else if (video.classList.contains("v4")) {
-        logo.classList.remove("brown-shadow");
-      }
+      // if (video.classList.contains("v1")) {
+      //   logo.classList.remove("golden-shadow");
+      // } else if (video.classList.contains("v2")) {
+      //   logo.classList.remove("blue-shadow");
+      // } else if (video.classList.contains("v3")) {
+      //   logo.classList.remove("gray-shadow");
+      // } else if (video.classList.contains("v4")) {
+      //   logo.classList.remove("brown-shadow");
+      // }
     });
   });
 });

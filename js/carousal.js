d79af367bf0d0a41291785e21b4e7bca042dbc02
rami <rami.ahmad.abdullah@gmail.com>
2025-12-09 08:55:53 +0600

    sscc - 6

--- js/carousal.js
+++ js/carousal.js
@@ -1,38 +1,74 @@
-let carousals = document.querySelectorAll('.carousal');
+function makePreviousBtn() {
+  // PREVIOUS BUTTON
+  const previousBtn = document.createElement("button");
+  previousBtn.classList.add("control-btn");
+  previousBtn.id = "prev-btn";
+  previousBtn.innerHTML = "&#10094;";
 
-carousals.forEach(carousal => {
+  return previousBtn;
+}
 
-    // GETTING ALL IMAGES INSIDE THE CAROUSAL
-    const carousalImages = carousal.querySelectorAll('.image');
+function makeNextBtn() {
+  // NEXT BUTTON
+  const nextBtn = document.createElement("button");
+  nextBtn.classList.add("control-btn");
+  nextBtn.id = "next-btn";
+  nextBtn.innerHTML = "&#10095;";
 
-    // CREATING CAROUSAL CONTROLS
-    const carousalControls = document.createElement('div');
-    carousalControls.classList.add('carousal-controls');
+  return nextBtn;
+}
 
-    // PREVIOUS BUTTON
-    const controlBtn = document.createElement('button');
-    controlBtn.classList.add('control-btn');
-    controlBtn.id = 'prev-btn';
-    controlBtn.innerHTML = '&#10094;';
+function makeProgressBar() {
+  // PROGRESS BAR FOR EACH IMAGE
+  const progressBar = document.createElement("div");
+  progressBar.classList.add("progress-bar");
 
-    // APPENDING PREVIOUS BUTTON TO CAROUSAL CONTROLS
-    carousalControls.appendChild(controlBtn);
+  // PROGRESS VALUE FOR EACH PROGRESS BAR
+  const progressValue = document.createElement("div");
+  progressValue.classList.add("progress-value");
 
-    const progressOfImages = document.createElement('div');
-    progressOfImages.classList.add('progress-of-images');
+  // ADDING PROGRESS VALUE TO PROGRESS BAR
+  progressBar.appendChild(progressValue);
 
-    const progressElement = `<div class="progress-item">
-                    <div class="progress-bar">
-                        <div class="progress-value"></div>
-                    </div>
-                </div>`;
+  return progressBar;
+}
 
-    carousalImages.forEach((image, index) => {
-        progressOfImages.appendChild(progressElement);
-    });
+// GET ALL THE CAROUSALS IN THE DOCUMENT
+let carousals = document.querySelectorAll(".carousal");
 
+carousals.forEach((carousal) => {
+  // GETTING ALL IMAGES INSIDE THE CAROUSAL
+  const carousalImages = carousal.querySelectorAll(".image");
 
+  // CREATING CAROUSAL CONTROLS CONTAINER
+  const carousalControls = document.createElement("div");
+  carousalControls.classList.add("carousal-controls");
 
-    console.log(carousalImages.length);
+  // APPENDING PREVIOUS BUTTON TO CAROUSAL CONTROLS
+  carousalControls.appendChild(makePreviousBtn());
 
+  // CREATING THE CONTAINER THAT WILL HOLD ALL PROGRESS BARS
+  const progressContainer = document.createElement("div");
+  progressContainer.classList.add("progress-container");
+
+  // APPENDING A PROGRESS ITEM FOR EACH IMAGE IN THE CAROUSAL
+  carousalImages.forEach((image, index) => {
+    // PROGRESSES THAT WILL BE ADDED TO EACH CAROUSAL
+    const progressItem = document.createElement("div");
+    progressItem.classList.add("progress-item");
+    progressItem.id = `image-${index}`;
+
+    // ADDING PROGRESS BAR TO PROGRESS ITEM WHICH WILL BE ADDED DEPENDING ON THE IMAGES COUNT IN THE CAROUSAL
+    progressItem.appendChild(makeProgressBar());
+    progressContainer.appendChild(progressItem);
+  });
+
+  // APPENDING THE WHOLE PROGRESS CONTAINER TO THE CAROUSAL CONTROLS DIV
+  carousalControls.appendChild(progressContainer);
+
+  // APPENDING THE NEXT BUTTON AT LAST
+  carousalControls.appendChild(makeNextBtn());
+
+  // APPENDING EVERYTHING TO THE CAROUSAL ITSELF
+  carousal.appendChild(carousalControls);
 });
